06e3c8f544b7ec208978f0fce5acdc046197dd68
YuHyeonWook <yho7955@naver.com>
2024-05-27 18:36:27 +0900

    feat: firebase 설치 및 설정 추가함

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCTHTRKxdRHshjEsZ2jt0m9jmcrO9QKhSM',
+  authDomain: 'toy2-abff4.firebaseapp.com',
+  projectId: 'toy2-abff4',
+  storageBucket: 'toy2-abff4.appspot.com',
+  messagingSenderId: '690501479513',
+  appId: '1:690501479513:web:be27c7fe6dda9dfbe0d548',
+  measurementId: 'G-7ZYRKL79WJ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export { app, analytics };
