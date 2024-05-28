90e2324156a42a5fd15db9f9e38811115928c78a
YuHyeonWook <yho7955@naver.com>
2024-05-28 12:19:20 +0900

    feat: 로그아웃 기능 구현

--- src/components/LoginOut.tsx
+++ src/components/LoginOut.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const LoginOut = () => {
-  return <div>LoginOut</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    try {
+      await signOut(auth);
+      localStorage.removeItem('user');
+      navigate('/');
+    } catch (error) {
+      console.log('로그아웃 실패:', error);
+    }
+  };
+
+  return <button onClick={handleLogOut}>logout</button>;
 };
 
 export default LoginOut;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { User } from 'firebase/auth';
+import LoginOut from '@/components/LoginOut';
 
 const MyPage = () => {
   const [user, setUser] = useState<User | null>(null);
@@ -13,6 +14,7 @@ const MyPage = () => {
 
   return (
     <div>
+      <LoginOut />
       <h2>My Page</h2>
       {user ? (
         <>
