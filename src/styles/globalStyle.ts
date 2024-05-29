389bcdc94a6fc61fed2f3f0ab5efabca8dfec1b1
minnug <parkminnug@gmail.com>
2024-05-29 21:14:11 +0900

    design: 전역 스타일 수정(폰트, 색상 지정)

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,20 +1,88 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    src: local('PretendardRegular');
+    src: url('../assets/fonts/PretendardRegular.woff') format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  } 
 
-:root {
-  --color-yellow: #FCD118;
-  font-size: 16px;
+  @font-face {
+    font-family: 'Pretendard';
+    src: local('PretendardMedium');
+    src: url('../assets/fonts/PretendardMedium.woff') format('truetype');
+    font-weight: 500;
+    font-style: normal;
+  } 
 
-}
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-body, #root {
-    font-family: "Helvetica", "Arial", sans-serif;
+  @font-face {
+    font-family: 'Pretendard';
+    src: local('../assets/fonts/PretendardSemiBold.woff');
+    src: url('../assets/fonts/PretendardSemiBold.woff') format('truetype');
+    font-weight: 600;
+    font-style: normal;
+  } 
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: local('PretendardBold');
+    src: url('../assets/fonts/PretendardBold.woff') format('truetype');
+    font-weight: 700;
+    font-style: normal;
+  } 
+
+  :root {
+    --color-green: #4cd964
+    --color-lightGreen: #f9fffa;
+    --color-gray: #969696;
+    --color-ligthGray: #c7c7c7;
+    --color-darkGray: #5e5e5e;
+    --color-black: #171717;
+    --color-white: #ffffff;
+
+    --border-gray: 1px solid #c7c7c7; 
+
+    font-size: 16px;
   }
 
+  * {
+      margin: 0;
+      padding: 0;
+      font-family: "Pretendard", "Helvetica", "Arial", sans-serif;
+      box-sizing: border-box;
+  }
+  
+  body {
+    font-family: "Pretendard", "Helvetica", "Arial", sans-serif;
+    font-size: 16px;
+    color: var(--color-black);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    padding: 0;
+    border: 0;
+    background: transparent;
+    cursor: pointer;
+  }
+
+  ul,
+  ol {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
 `;
+
 export default GlobalStyle;
