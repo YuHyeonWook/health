d09b623be5eef207990b669af73947015f0dd7f2
minnug <parkminnug@gmail.com>
2024-05-31 20:54:38 +0900

    Design: ApplyForm 페이지 수정

--- src/pages/ApplyForm.tsx
+++ src/pages/ApplyForm.tsx
@@ -1,32 +1,62 @@
 import Button from '@/components/Button';
+import FormInput from '@/components/FormInput';
+import FormRadio from '@/components/FormRadio';
+import FormRadioGroup from '@/components/FormRadioGroup';
 import Layout from '@/components/layout/Layout';
 import styled from 'styled-components';
 
 const ApplyForm: React.FC = () => {
   return (
     <Layout>
-      <h2>PT 신청</h2>
+      <Title>PT 신청</Title>
       <form>
-        <div className="input-box">
-          <label>시작 날짜</label>
-          <input />
-        </div>
-        <div className="input-box">
-          <label>횟수</label>
-          <input />
-        </div>
-        <div className="input-box">
-          <label>퍼스널 트레이너</label>
-          <input />
-        </div>
-        <div className="input-box">
-          <label>비용 (1회 - 5만원)</label>
-          <input type="text" />
-        </div>
-        <Button>신청하기</Button>
+        <FormInput type="date" label="시작 날짜" value="dateStart" />
+        <FormRadioGroup label="횟수">
+          <FormRadio name="number" value="10" defaultChecked>
+            10회
+          </FormRadio>
+          <FormRadio name="number" value="15">
+            15회
+          </FormRadio>
+          <FormRadio name="number" value="20">
+            20회
+          </FormRadio>
+          <FormRadio name="number" value="25">
+            25회
+          </FormRadio>
+          <FormRadio name="number" value="30">
+            30회
+          </FormRadio>
+        </FormRadioGroup>
+        <FormRadioGroup label="퍼스널 트레이너">
+          <FormRadio name="trainer" value="trainer1" defaultChecked>
+            박민주 T
+          </FormRadio>
+          <FormRadio name="trainer" value="trainer2">
+            유현욱 T
+          </FormRadio>
+          <FormRadio name="trainer" value="trainer3">
+            이동희 T
+          </FormRadio>
+          <FormRadio name="trainer" value="trainer4">
+            정보현 T
+          </FormRadio>
+        </FormRadioGroup>
+        <FormInput type="text" label="비용 (1회 - 5만원)" value="" />
+        <ApplyBtn>신청하기</ApplyBtn>
       </form>
     </Layout>
   );
 };
+
+const Title = styled.h2`
+  margin-bottom: 4rem;
+  font-size: 2.8rem;
+  font-weight: 700;
+`;
+
+const ApplyBtn = styled(Button)`
+  margin: 6rem auto 0;
+`;
 
 export default ApplyForm;
