8926743701aadba41ba13f1132f2cb784a9c1c55
minnug <parkminnug@gmail.com>
2024-06-08 21:06:07 +0900

    Chore: firebase env 설정

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCTHTRKxdRHshjEsZ2jt0m9jmcrO9QKhSM',
-  authDomain: 'toy2-abff4.firebaseapp.com',
-  databaseURL: 'https://toy2-abff4-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'toy2-abff4',
-  storageBucket: 'toy2-abff4.appspot.com',
-  messagingSenderId: '690501479513',
-  appId: '1:690501479513:web:3268869662344392e0d548',
-  measurementId: 'G-8YNJSL2EFE',
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
